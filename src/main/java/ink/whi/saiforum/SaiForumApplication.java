3ce1eb4157e690cd23073d5287bf2bebc296933f
qing-wq <2702461713@qq.com>
2023-04-25 21:58:47 +0800

    init: 项目初始化

---
+++ src/main/java/ink/whi/saiforum/SaiForumApplication.java
@@ -0,0 +1,13 @@
+package ink.whi.saiforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaiForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaiForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ink/whi/saiforum/SaiForumApplicationTests.java
@@ -0,0 +1,13 @@
+package ink.whi.saiforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaiForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
