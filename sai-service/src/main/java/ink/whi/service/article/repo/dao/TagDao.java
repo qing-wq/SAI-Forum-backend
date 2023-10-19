53bfc32973a491a8db22d69baa8771dd9daf70ec
qing-wq <2702461713@qq.com>
2023-10-19 15:09:38 +0800

    修复tags状态问题

--- sai-service/src/main/java/ink/whi/service/article/repo/dao/TagDao.java
+++ sai-service/src/main/java/ink/whi/service/article/repo/dao/TagDao.java
@@ -2,6 +2,7 @@ package ink.whi.service.article.repo.dao;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import ink.whi.api.model.base.BaseDO;
+import ink.whi.api.model.enums.PushStatusEnum;
 import ink.whi.api.model.enums.YesOrNoEnum;
 import ink.whi.api.model.vo.page.PageParam;
 import ink.whi.api.model.vo.article.dto.TagDTO;
@@ -20,15 +21,11 @@ import java.util.List;
 public class TagDao extends ServiceImpl<TagMapper, TagDO> {
 
     public List<TagDTO> listTags(PageParam pageParam) {
-        List<TagDO> list = lambdaQuery().last(PageParam.getLimitSql(pageParam))
-                .orderByDesc(BaseDO::getCreateTime)
-                .list();
-        return ArticleConverter.toDtoList(list);
-    }
-
-    public List<TagDTO> listTag(PageParam pageParam) {
-        List<TagDO> list = lambdaQuery().eq(TagDO::getDeleted, YesOrNoEnum.NO.getCode())
+        List<TagDO> list = lambdaQuery()
+                .eq(TagDO::getStatus, PushStatusEnum.ONLINE.getCode())
+                .eq(TagDO::getDeleted, YesOrNoEnum.NO.getCode())
                 .last(PageParam.getLimitSql(pageParam))
+                .orderByDesc(BaseDO::getCreateTime)
                 .list();
         return ArticleConverter.toDtoList(list);
     }
--- sai-service/src/main/java/ink/whi/service/article/service/Impl/TagSettingServiceImpl.java
+++ sai-service/src/main/java/ink/whi/service/article/service/Impl/TagSettingServiceImpl.java
@@ -58,7 +58,7 @@ public class TagSettingServiceImpl implements TagSettingService {
 
     @Override
     public PageVo<TagDTO> getTagList(PageParam pageParam) {
-        List<TagDTO> tagDTOS = tagDao.listTag(pageParam);
+        List<TagDTO> tagDTOS = tagDao.listTags(pageParam);
         Integer totalCount = tagDao.countTag();
         return PageVo.build(tagDTOS, pageParam.getPageSize(), pageParam.getPageNum(), totalCount);
     }
